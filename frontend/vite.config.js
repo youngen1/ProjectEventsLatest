a2df74caba7ba0d788f8b03c116f89c998af6297
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-06-06 03:38:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/frontend/dist/', // Explicitly set base path
+    base: '/', 
     define: {
       'process.env': env,
     },
