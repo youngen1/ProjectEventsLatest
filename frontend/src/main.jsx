fb496e480d936cfedc91555dc7e0d99ed7c4f86f
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-06-06 10:47:06 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import './index.css';
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { LoadScript } from "@react-google-maps/api";
+
+const libraries = ["places"];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <LoadScript
+        googleMapsApiKey={import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        libraries={libraries}
+        loadingElement={<div>Loading...</div>}
+        onLoad={(data) => console.log("Google Maps Loaded", data)}
+        onError={(err) => console.error("Error loading Google Maps", err)}
+      >
+        <App />
+      </LoadScript>
     </AuthProvider>
   </React.StrictMode>
 );
