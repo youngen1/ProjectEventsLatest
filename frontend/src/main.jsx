42624e0c1ed7b18cbefcaf683787a4a18e1186f4
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-06-06 11:04:14 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <LoadScript
-        googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+        googleMapsApiKey="AIzaSyAsvpRyz_UMpIJ63M86Yj-oSEHZlCQlN0o"
         libraries={libraries}
         loadingElement={<div>Loading...</div>}
         onLoad={(data) => console.log("Google Maps Loaded", data)}
