735f50fbcdc572fc7aef8e5e20bdfd2d36f20172
youngen1 <122107840+youngen1@users.noreply.github.com>
2025-06-06 10:58:32 +0530

    Update loadGoogleMaps.js

--- frontend/src/loadGoogleMaps.js
+++ frontend/src/loadGoogleMaps.js
@@ -1,6 +1,6 @@
 const loadGoogleMaps = () => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
